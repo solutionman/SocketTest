8d9c59d8f74238bb04d34c8f0260bb62577048b3
lom <solutionman@inbox.ru>
2022-04-10 05:31:47 -0400

    clean up

--- pingTest.c
+++ pingTest.c
@@ -8,27 +8,22 @@ int main(int argc, char *argv[]){
     char *hostname = "www.google.com";
     char ip[100];
     struct hostent *he;
-    struct in_addr **addr_list;    
-    
-    printf("debug\n");    
+    struct in_addr **addr_list;       
     
     if( (he = gethostbyname( hostname ) ) == NULL ) {
         // gethostbyname failed
         herror("gethostbyname");
         return 1;
     }
-    printf("debug2\n");
     
     addr_list = (struct in_addr **) he -> h_addr_list;
     
-    printf("debug3\n");
     // cast h_addr_list to in_addr
     int i;
     for(i = 0; addr_list[i] != NULL; i++){
         // return first
         strcpy(ip, inet_ntoa(*addr_list[i]) );
     }
-    printf("debug4\n");
     
     printf( "%s resolved to : %s", hostname, ip );
     return 0;
